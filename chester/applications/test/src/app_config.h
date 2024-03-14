322c9737f502c8417de98820e33b745f78b01146
Frese <frese@iotconecta.com.br>
2024-03-14 18:31:49 -0300

    Added feature project.yaml generation from empty folder

---
+++ chester/applications/test/src/app_config.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023 HARDWARIO a.s.
+ *
+ * SPDX-License-Identifier: LicenseRef-HARDWARIO-5-Clause
+ */
+
+#ifndef APP_CONFIG_H_
+#define APP_CONFIG_H_
+
+/* Includes ------------------------------------------------------------------*/
+
+/* Private includes --------------------------------------------------------------------*/
+
+/* Zephyr includes */
+#include <zephyr/shell/shell.h>
+
+/* Standard includes */
+#include <stdbool.h>
+#include <stddef.h>
+
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Private Variables -------------------------------------------------------------------*/
+
+enum app_config_mode {
+	APP_CONFIG_MODE_NONE = 0,
+	APP_CONFIG_MODE_LTE = 1,
+	APP_CONFIG_MODE_LRW = 2,
+};
+
+struct app_config {
+    int interval_report;
+    char apn [63 + 1];
+    float temperature;
+    bool backup_report_connected;
+};
+
+extern struct app_config g_app_config;
+
+/* USER CODE BEGIN Variables */
+/* USER CODE END Variables */
+
+/* Private Functions -------------------------------------------------------------------*/
+
+int app_config_cmd_config_show(const struct shell *shell, size_t argc, char **argv);
+int app_config_cmd_config_interval_report(const struct shell *shell, size_t argc, char **argv);
+int app_config_cmd_config_temperature(const struct shell *shell, size_t argc, char **argv);
+int app_config_cmd_config_backup_report_connected(const struct shell *shell, size_t argc, char **argv);
+
+/* USER CODE BEGIN Functions 1 */
+/* USER CODE END Functions 1 */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APP_CONFIG_H_ */
---
+++ chester/applications/test/src/app_shell.c
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2023 HARDWARIO a.s.
+ *
+ * SPDX-License-Identifier: LicenseRef-HARDWARIO-5-Clause
+ */
+ 
+/* Includes ------------------------------------------------------------------*/
+
+#include "app_config.h"
+
+/* Private includes --------------------------------------------------------------------*/
+
+/* Zephyr includes */
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/shell/shell.h>
+
+/* Standard includes */
+#include <errno.h>
+#include <stdlib.h>
+
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
+LOG_MODULE_REGISTER(app_shell, LOG_LEVEL_INF);
+
+/* USER CODE BEGIN Variables */
+/* USER CODE END Variables */
+
+static int print_help(const struct shell *shell, size_t argc, char **argv)
+{
+    if (argc > 1) {
+        shell_error(shell, "command not found: %s", argv[1]);
+        shell_help(shell);
+        return -EINVAL;
+    }
+
+    shell_help(shell);
+    return 0;
+}
+
+/* clang-format off */
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+    sub_app_config,
+   
+    SHELL_CMD_ARG(show, NULL,
+                  "List current configuration.",
+                  app_config_cmd_config_show, 1, 0),
+
+    SHELL_CMD_ARG(interval-report, NULL,
+                  "Set report interval (default: 3600)",
+                  app_config_cmd_config_interval_report, 1, 1),
+
+    SHELL_CMD_ARG(apn, NULL,
+                  "Set APN network name",
+                  app_config_cmd_config_apn, 1, 1),
+
+    SHELL_CMD_ARG(temperature, NULL,
+                  "Set Temperature 0.0f to 50.f",
+                  app_config_cmd_config_temperature, 1, 1),
+
+    SHELL_CMD_ARG(backup-report-connected, NULL,
+                  "Set backup report connected (default: true)",
+                  app_config_cmd_config_backup_report_connected, 1, 1),
+
+/* USER CODE BEGIN Functions 1 */
+/* USER CODE END Functions 1 */
+
+    SHELL_SUBCMD_SET_END
+);
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+    sub_app,
+
+    SHELL_CMD_ARG(config, &sub_app_config, "Configuration commands.",
+                  print_help, 1, 0),
+        
+    SHELL_SUBCMD_SET_END
+
+/* USER CODE BEGIN Functions 2 */
+/* USER CODE END Functions 2 */
+
+);
+
+SHELL_CMD_REGISTER(app, &sub_app, "Application commands.", print_help);
+    SHELL_CMD_REGISTER(send, NULL,
+                       "Set report interval (default: 3600)",
+                       app_send, 1, 1);
+
+/* USER CODE BEGIN Functions 3 */
+/* USER CODE END Functions 3 */
+
+/* clang-format on */
